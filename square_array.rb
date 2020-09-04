71addc07a45e16c86978ea58b38c9deadd723496
Ben Altman <altmanguitar@gmail.com>
2020-09-04 20:00:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 
-def square_array(array)
-  square_array = []
-  array.each do |number|
-    square_array.push(number**2)
+def square_array(array) #signature method accepts an array argument
+  square_array = [] #define new array
+  array.each do |number| #operate on array
+    square_array.push(number**2) #push output into new array
   end
-  square_array
+  square_array #call new array as output
 end
 
 #this is an alternative method:
